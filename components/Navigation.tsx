c2a692606259cb01d5bc1c8ae14f3dbfbd63c3fd
karlplank93 <karlplank93@gmail.com>
2026-03-11 09:59:59 +1300

    fix

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -33,7 +33,7 @@ export default function Navigation() {
 
           {/* Logo */}
           <Link href="/" className="flex-shrink-0">
-            <img src="/base-mortgages-logo.svg" alt="Base Mortgages" className="h-10 w-auto" />
+            <img src="/base-mortgages-logo.svg" alt="Base Mortgages" className="h-10 w-auto brightness-0 invert" />
           </Link>
 
           {/* Desktop nav links */}
