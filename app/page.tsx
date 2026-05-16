b913b06b7bfa953b3ff77048e7597725842e6ab5
karlplank93 <karlplank93@gmail.com>
2026-05-16 21:16:23 +1200

    8

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import Hero from "@/components/Hero";
 import HowWeHelp from "@/components/HowWeHelp";
-import BankStrip from "@/components/BankStrip";
-import Process from "@/components/Process";
 import MeetAdviser from "@/components/MeetAdviser";
 import Reviews from "@/components/Reviews";
-import Lenders from "@/components/Lenders";
-import Services from "@/components/Services";
 import Pricing from "@/components/Pricing";
 import FAQ from "@/components/FAQ";
 import CTABanner from "@/components/CTABanner";
@@ -15,12 +11,8 @@ export default function Home() {
     <>
       <Hero />
       <HowWeHelp />
-      <BankStrip />
-      <Process />
       <MeetAdviser />
       <Reviews />
-      <Lenders />
-      <Services />
       <Pricing />
       <FAQ />
       <CTABanner />
